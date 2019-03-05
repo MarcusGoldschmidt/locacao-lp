a39d3be4c6e735f18e6d681d46061ad0e86ec714
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-03-05 14:40:35 -0400

    Corrigido nome da funcao

--- main.c
+++ main.c
@@ -4,23 +4,24 @@
 #include "./tela/carro/telacarro.h"
 #include "./tela/cliente/telacliente.h"
 
-void main(){
+int main(){
     int menu = 0;
     while (menu != 9){  
         printf("# MENU PRINCIPAL \n  [1]Locações \n  [2]Clientes \n  [3]Carros \n[9]Sair \n  Opção ");
         scanf("%d",&menu);
         if(menu == 1){
-			System("cls");
+			system("cls");
 			telaLocacao();			
 		}
 		if(menu == 2){
-			System("cls");
+			system("cls");
 			telaCliente();
 		}
 		if(menu == 3){
-			System("cls");
-			telaCarros();
+			system("cls");
+			telaCarro();
 		}
         else{printf("Comando não conhecido.\nTente Novamente.\n");}     
     }
+    return 0;
 }
